72d62e333c9be3a38ad8dd695a92e1d07a06d13c
Walter000 <liangzong1995@163.com>
2018-04-29 15:15:25 +0800

    Update baseline_girdSearch.py

--- baseline_girdSearch.py
+++ baseline_girdSearch.py
@@ -3,7 +3,7 @@
 # github: https://github.com/Walter000/tencent_competition
 
 """
- 在作者Baseline代码基础上增加了网格搜索，同时直接读取sparse格式的训练数，并且直接读入训练标签，不用再次读入所有训练数据，
+ 在作者Baseline代码基础上增加了随机搜索，同时直接读取sparse格式的训练数，并且直接读入训练标签，不用再次读入所有训练数据，
  最后保存预测的结果和最好的模型best_estimator, 同时将feature_importance写入文件方面后期分析
 """
 import pandas as pd
